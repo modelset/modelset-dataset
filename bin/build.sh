4786527e3eaf87a2754628e2490853dd1d6016ba
Jesús Sánchez Cuadrado <jesus.sanchez.cuadrado@gmail.com>
2021-11-22 12:42:11 +0100

    Improve build script to copy only selected files

--- bin/build.sh
+++ bin/build.sh
@@ -12,8 +12,31 @@ fi
 
 mkdir /tmp/modelset
 echo "Copying..."
-cp -r README.md datasets/ txt/ /tmp/modelset
-./bin/copy-files.py
+
+mkdir -p /tmp/modelset/datasets/dataset.ecore/data
+mkdir -p /tmp/modelset/datasets/dataset.ecore/src/dataset/ecore/
+mkdir -p /tmp/modelset/datasets/dataset.genmymodel/data
+mkdir -p /tmp/modelset/datasets/dataset.genmymodel/src/dataset/genmymodel/
+
+
+cp datasets/dataset.ecore/.project /tmp/modelset/datasets/dataset.ecore
+cp datasets/dataset.ecore/build.properties /tmp/modelset/datasets/dataset.ecore
+# cp datasets/dataset.ecore/.settings /tmp/modelset/datasets/dataset.ecore -r
+cp datasets/dataset.ecore/META-INF /tmp/modelset/datasets/dataset.ecore -r
+cp datasets/dataset.ecore/data/ecore.db /tmp/modelset/datasets/dataset.ecore/data/ecore.db
+cp datasets/dataset.ecore/data/analysis.db /tmp/modelset/datasets/dataset.ecore/data/analysis.db
+cp datasets/dataset.ecore/src/dataset/ecore/ValidateMetadata.java /tmp/modelset/datasets/dataset.ecore/src/dataset/ecore/
+
+cp datasets/dataset.genmymodel/.project /tmp/modelset/datasets/dataset.genmymodel
+cp datasets/dataset.genmymodel/build.properties /tmp/modelset/datasets/dataset.genmymodel
+cp datasets/dataset.genmymodel/data/genmymodel.db /tmp/modelset/datasets/dataset.genmymodel/data/genmymodel.db
+# cp dataset.genmymodel/data/analysis.db /tmp/modelset/datasets/dataset.genmymodel/data/analysis.db
+#cp datasets/dataset.genmymodel/.settings /tmp/modelset/datasets/dataset.genmymodel -r
+cp datasets/dataset.genmymodel/META-INF /tmp/modelset/datasets/dataset.genmymodel -r
+cp datasets/dataset.genmymodel/src/dataset/genmymodel/ValidateMetadata.java  /tmp/modelset/datasets/dataset.genmymodel/src/dataset/genmymodel/ValidateMetadata.java 
+
+cp -r README.md txt/ /tmp/modelset
+./bin/copy-files.py .
 echo "Zipping..."
 cd /tmp
 zip -r modelset.zip modelset
